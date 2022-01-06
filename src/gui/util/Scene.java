c82b17f9850c1c666524c3be49780cba0c81d85c
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2022-01-06 09:08:44 +0100

    Added sceneswapper

--- src/gui/util/Scene.java
+++
@@ -1,4 +0,0 @@
-package gui.util;
-
-public class Scene {
-}
---
+++ src/gui/util/SceneSwapper.java
@@ -0,0 +1,35 @@
+package gui.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+
+public class SceneSwapper {
+
+    /**
+     * switches the stage to a certain fxml file.
+     * @param stage of stage wanted to be shown.
+     * @param fxmlClassName the file that wanted to be shown
+     */
+    public void sceneSwitch(Stage stage, String fxmlClassName){
+        try {
+            URL url = new File("src/gui/view/" + fxmlClassName).toURI().toURL();
+            Parent Scene = FXMLLoader.load(url);
+            Scene ViewScene = new Scene(Scene);
+            stage.setTitle("Private Movie Collection");
+            //Image image = new Image("/gui/Images/MyTunesLogo.png");
+            //stage.getIcons().add(image);
+            stage.setScene(ViewScene);
+            stage.show();
+        }catch (IOException IOex){
+            System.out.println("Can't swap scene");
+        }
+    }
+}
